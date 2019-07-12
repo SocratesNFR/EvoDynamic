35c220d0d8f0ff5f07574841ff8a777b1f4f833d
Sidney Pontes-Filho <sidneypontesf@gmail.com>
2019-07-12 17:12:40 +0200

    Update run_evolve.py

--- examples/run_evolve.py
+++ examples/run_evolve.py
@@ -1,3 +1,15 @@
 import os
 os.system("python3 evolve_cellular_automata_1d_criticality.py evolve_deterministic_ca1d.csv")
 os.system("python3 evolve_stochastic_ca_1d_criticality.py evolve_stochastic_ca1d.csv")
+
+os.system("python3 evolve_cellular_automata_1d_criticality.py evolve_deterministic_ca1d.csv")
+os.system("python3 evolve_stochastic_ca_1d_criticality.py evolve_stochastic_ca1d.csv")
+
+os.system("python3 evolve_cellular_automata_1d_criticality.py evolve_deterministic_ca1d.csv")
+os.system("python3 evolve_stochastic_ca_1d_criticality.py evolve_stochastic_ca1d.csv")
+
+os.system("python3 evolve_cellular_automata_1d_criticality.py evolve_deterministic_ca1d.csv")
+os.system("python3 evolve_stochastic_ca_1d_criticality.py evolve_stochastic_ca1d.csv")
+
+os.system("python3 evolve_cellular_automata_1d_criticality.py evolve_deterministic_ca1d.csv")
+os.system("python3 evolve_stochastic_ca_1d_criticality.py evolve_stochastic_ca1d.csv")
