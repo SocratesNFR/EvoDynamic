a8b4146780e8f76459c78ba21c58f0eee78ce7e1
Sidney Pontes-Filho <sidneypontesf@gmail.com>
2020-06-24 15:15:25 +0200

    Fix memory for reservoir computing

--- evodynamic/experiment/memory.py
+++ evodynamic/experiment/memory.py
@@ -1,37 +1,54 @@
 """ State memory """
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
+import numpy as np
 
 class Memory(object):
   def __init__(self, experiment, state, memory_size) -> None:
+    if memory_size < 2:
+      raise Exception("Error: Memory size must be equal or larger than 1.")
     self.experiment = experiment
     self.state = state
+    self.state_shape = self.state.shape
     self.memory_size = memory_size
-    self.state_memory = []
-    for i in range(memory_size):
-      self.state_memory.append(tf.get_variable(
-          state.name.replace(":0", "")+"_"+str(i),
-          initializer=tf.zeros(self.state.shape, dtype=tf.dtypes.float64)))
-
-    self.memory_op = tf.concat(self.state_memory,0)
+    self.memory_shape = tuple([self.memory_size*self.state_shape[0]] +
+                              list(np.array(self.state_shape)[1:]))
+    self.state_memory = tf.get_variable(
+          state.name.replace(":0", "")+"_memory",
+          initializer=tf.zeros(self.memory_shape, dtype=tf.dtypes.float64))
 
   def get_op(self):
-    return self.memory_op
+    return self.state_memory
 
   def get_state_memory(self):
-    return self.experiment.session.run(self.memory_op)
+    return self.experiment.session.run(self.state_memory)
 
   def reset(self):
-    for state_memory_part in self.state_memory:
-      self.experiment.session.run(tf.assign(state_memory_part,
-                                            tf.zeros_like(state_memory_part)))
+    self.experiment.session.run(tf.assign(self.state_memory,
+                                          tf.zeros_like(self.state_memory)))
 
   def update_state_memory(self):
     if self.experiment.memory_counter < self.memory_size:
+#      update_indices = list(range(self.experiment.memory_counter*self.state_shape[0],
+#                                  (self.experiment.memory_counter+1)*self.state_shape[0]))
+
+      update_indices = [i for i in range(self.experiment.memory_counter*self.state_shape[0],\
+                        (self.experiment.memory_counter+1)*self.state_shape[0])]
+
       self.experiment.session.run(
-          tf.assign(self.state_memory[self.experiment.memory_counter], self.state))
+          tf.scatter_update(self.state_memory, update_indices, self.state))
+
     else:
-      state_memory_part = tf.Variable(self.state_memory.pop(0))
-      self.experiment.session.run(tf.assign(state_memory_part, self.state))
-      self.state_memory.append(state_memory_part)
+      selection_indices = [[i] for i in range(self.state_shape[0],\
+                           self.memory_size*self.state_shape[0])]
+
+      memory_shift_selection_op = tf.gather_nd(self.state_memory,selection_indices)
+
+      state_memory_update_indices = [i for i in range(self.memory_size*self.state_shape[0])]
+      concat_op = tf.concat([memory_shift_selection_op, self.state], 0)
+
+      state_memory_update_op = tf.scatter_update(self.state_memory,
+                                                 state_memory_update_indices,
+                                                 concat_op)
+      self.experiment.session.run(state_memory_update_op)
 
