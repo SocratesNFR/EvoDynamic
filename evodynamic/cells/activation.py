195d9647c59c537ad45badc590dcd9d06a159a0d
Sidney Pontes-Filho <sidneypontesf@gmail.com>
2021-08-16 13:51:20 +0200

    Add docstring in scripts in evodynamic.cells

--- evodynamic/cells/activation.py
+++ evodynamic/cells/activation.py
@@ -4,6 +4,35 @@ import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 
 def game_of_life_func(count_neighbors, previous_state):
+  """
+  game_of_life_func(count_neighbors, previous_state)
+
+  Activation function for Conway's Game of Life.
+
+  Parameters
+  ----------
+  count_neighbors : Tensor
+      Result of the matrix multiplication that happens in
+      evodynamic.connection.WeightedConnection
+  previous_state : Tensor
+      State of a binary state of a group of cells before being updated.
+
+  Returns
+  -------
+  out : Tensor
+      Tensor containing the operation for changing the binary states of a group
+      of cells as the Conway's Game of Life.
+
+  Notes
+  -----
+  The values of the first two parameters come from operations in
+  the Connection object.
+
+  Examples
+  --------
+  >>> evodynamic.connection.WeightedConnection(ca_binary_state,ca_binary_state,
+                                               game_of_life_func,ca_conn)
+  """
   born_cells_op = tf.equal(count_neighbors, 3)
   kill_cells_sub_op = tf.less(count_neighbors, 2)
   kill_cells_over_op = tf.greater(count_neighbors, 3)
@@ -16,6 +45,42 @@ def game_of_life_func(count_neighbors, previous_state):
   return tf.where(born_cells_op, tf.ones(tf.shape(previous_state), dtype=tf.float64), update_kill_op)
 
 def rule_binary_ca_1d_width3_func(pattern, previous_state, rule):
+  """
+  rule_binary_ca_1d_width3_func(pattern, previous_state, rule)
+
+  Activation function for 3-neighbor Elementary Cellular Automaton.
+
+  Parameters
+  ----------
+  pattern : Tensor
+      Result of the matrix multiplication that happens in
+      evodynamic.connection.WeightedConnection
+  previous_state : Tensor
+      State of a binary state of a group of cells before being updated.
+  rule: int between 0 and 255
+      rule number for the 3-neighbor Elementary Cellular Automaton.
+
+  Returns
+  -------
+  out : Tensor
+      Tensor containing the operation for changing the binary states of a group
+      of cells as expressed by the chosen rule of the elementary celullar
+      automaton.
+
+  Notes
+  -----
+  The values of the first two parameters come from operations in
+  the Connection object. The activation functions with more the two parameters
+  must have the extra parameters passed through the parameter 'fargs_list' of
+  'evodynamic.connection.WeightedConnection'.
+
+  Examples
+  --------
+  >>> evodynamic.connection.WeightedConnection(ca_binary_state,ca_binary_state,
+                                               rule_binary_ca_1d_width3_func,
+                                               ca_conn,
+                                               fargs_list=[(rule,)])
+  """
   pattern_0_op = tf.equal(pattern, 0)
   pattern_1_op = tf.equal(pattern, 1)
   pattern_2_op = tf.equal(pattern, 2)
@@ -62,6 +127,41 @@ def rule_binary_ca_1d_width3_func(pattern, previous_state, rule):
   return tf.where(pattern_7_op, new_state_pattern_7, update_6_op)
 
 def rule_binary_sca_1d_width3_func(pattern, previous_state, prob_list):
+  """
+  rule_binary_sca_1d_width3_func(pattern, previous_state, prob_list)
+
+  Activation function for 3-neighbor Stochastic Elementary Cellular Automaton.
+
+  Parameters
+  ----------
+  pattern : Tensor
+      Result of the matrix multiplication that happens in
+      evodynamic.connection.WeightedConnection
+  previous_state : Tensor
+      State of a binary state of a group of cells before being updated.
+  prob_list: list of 8 float numbers between 0 and 1
+      Probability list for the 8 different neighborhood patterns.
+
+  Returns
+  -------
+  out : Tensor
+      Tensor containing the operation for changing the binary states of a group
+      of cells.
+
+  Notes
+  -----
+  The values of the first two parameters come from operations in
+  the Connection object. The activation functions with more the two parameters
+  must have the extra parameters passed through the parameter 'fargs_list' of
+  'evodynamic.connection.WeightedConnection'.
+
+  Examples
+  --------
+  >>> evodynamic.connection.WeightedConnection(ca_binary_state,ca_binary_state,
+                                               rule_binary_sca_1d_width3_func,
+                                               ca_conn,
+                                               fargs_list=[(prob_list,)])
+  """
   shape_previous_state = tf.shape(previous_state)
   pattern_0_op = tf.equal(pattern, 0)
   pattern_1_op = tf.equal(pattern, 1)
@@ -139,13 +239,103 @@ def rule_binary_sca_1d_width3_func(pattern, previous_state, prob_list):
   # Return update_7_op
   return tf.where(pattern_7_op, new_state_pattern_7, update_6_op)
 
-def sigmoid(x, args):
+def sigmoid(x, empty_parameter):
+  """
+  sigmoid(x, empty_parameter)
+
+  Sigmoid activation function. It works as a wrapped for EvoDynamic functions.
+
+  Parameters
+  ----------
+  x : Tensor
+      Input value or the result of the matrix multiplication that happens in
+      evodynamic.connection.WeightedConnection
+  empty_parameter : Tensor
+      Empty parameter for matching the necessary number of parameters in case
+      of using evodynamic.connection.WeightedConnection.
+
+  Returns
+  -------
+  out : Tensor
+      Activation function result.
+
+  Notes
+  -----
+  The values of the first two parameters come from operations in
+  the Connection object.
+
+  Examples
+  --------
+  >>> evodynamic.connection.WeightedConnection(ann_layer_1,ann_layer_2,
+                                               sigmoid,l1_l2_connection)
+  """
   return tf.sigmoid(x)
 
-def relu(x, args):
+def relu(x, empty_parameter):
+  """
+  relu(x, empty_parameter)
+
+  Rectified linear unit activation function. It works as a wrapped for
+  EvoDynamic functions.
+
+  Parameters
+  ----------
+  x : Tensor
+      Input value or the result of the matrix multiplication that happens in
+      evodynamic.connection.WeightedConnection
+  empty_parameter : Tensor
+      Empty parameter for matching the necessary number of parameters in case
+      of using evodynamic.connection.WeightedConnection.
+
+  Returns
+  -------
+  out : Tensor
+      Activation function result.
+
+  Notes
+  -----
+  The values of the first two parameters come from operations in
+  the Connection object.
+
+  Examples
+  --------
+  >>> evodynamic.connection.WeightedConnection(ann_layer_1,ann_layer_2,
+                                               relu,l1_l2_connection)
+  """
   return tf.nn.relu(x)
 
-def stochastic_sigmoid(x, args):
+def stochastic_sigmoid(x, empty_parameter):
+  """
+  stochastic_sigmoid(x, empty_parameter)
+
+  Stochastic sigmoid activation function. It uses the sigmoid result as the
+  probability of selecting a binary state.
+
+  Parameters
+  ----------
+  x : Tensor
+      Input value or the result of the matrix multiplication that happens in
+      evodynamic.connection.WeightedConnection
+  empty_parameter : Tensor
+      Empty parameter for matching the necessary number of parameters in case
+      of using evodynamic.connection.WeightedConnection.
+
+  Returns
+  -------
+  out : Tensor
+      Activation function result.
+
+  Notes
+  -----
+  The values of the first two parameters come from operations in
+  the Connection object.
+
+  Examples
+  --------
+  >>> evodynamic.connection.WeightedConnection(ann_layer_1,ann_layer_2,
+                                               stochastic_sigmoid,
+                                               l1_l2_connection)
+  """
   shape_x = tf.shape(x)
   prob = tf.sigmoid(x)
   prob_mask = tf.less_equal(tf.random.uniform(shape_x, dtype=tf.float64), prob)
@@ -154,6 +344,25 @@ def stochastic_sigmoid(x, args):
                  tf.zeros(shape_x, dtype=tf.float64))
 
 def _stochastic_prob(prob_mean, prob_std):
+  """
+  _stochastic_prob(prob_mean, prob_std)
+
+  Private stochastic probability generates a probability from a defined
+  normal distribution. This function is used by
+  'stochastic_prob'.
+
+  Parameters
+  ----------
+  prob_mean : Tensor
+      Mean of the normal distribution.
+  prob_std : Tensor
+      Standard deviation of the normal distribution.
+
+  Returns
+  -------
+  out : Tensor
+      Generated probability with value between 0 and 1.
+  """
   prob_mean_mod = tf.math.log(tf.div_no_nan(prob_mean, 1-prob_mean))
   #sample_random_normal = tf.random.normal([], prob_mean_mod, prob_std, seed=1)
   sample_random_normal = tf.random.normal([], prob_mean_mod, prob_std)
@@ -161,6 +370,25 @@ def _stochastic_prob(prob_mean, prob_std):
   return prob
 
 def stochastic_prob(prob_mean, prob_std):
+  """
+  stochastic_prob(prob_mean, prob_std)
+
+  Stochastic probability generates a probability from a defined
+  normal distribution. If prob_mean is 0 or 1, the value is returned unchanged.
+  This function is used by 'rule_binary_soc_sca_1d_width3_func'.
+
+  Parameters
+  ----------
+  prob_mean : Tensor
+      Mean of the normal distribution.
+  prob_std : Tensor
+      Standard deviation of the normal distribution.
+
+  Returns
+  -------
+  out : Tensor
+      Generated probability with value between 0 and 1.
+  """
   prob = tf.cond(tf.math.logical_and(tf.math.not_equal(prob_mean, 0),
                                      tf.math.not_equal(prob_mean, 1)),
                  true_fn=lambda: _stochastic_prob(prob_mean, prob_std),
@@ -169,91 +397,47 @@ def stochastic_prob(prob_mean, prob_std):
   return prob
 
 def rule_binary_soc_sca_1d_width3_func(pattern, previous_state, prob_list):
+  """
+  rule_binary_soc_sca_1d_width3_func(pattern, previous_state, prob_list)
+
+  Activation function for 3-neighbor Stochastic Elementary Cellular Automaton
+  for analysis of self-organized criticality.
+  Similar to 'rule_binary_sca_1d_width3_func', but if all states are 1, then
+  initialize randomly the state.
+
+  Parameters
+  ----------
+  pattern : Tensor
+      Result of the matrix multiplication that happens in
+      evodynamic.connection.WeightedConnection
+  previous_state : Tensor
+      State of a binary state of a group of cells before being updated.
+  prob_list: list of 8 float numbers between 0 and 1
+      Probability list for the 8 different neighborhood patterns.
+
+  Returns
+  -------
+  out : Tensor
+      Tensor containing the operation for changing the binary states of a group
+      of cells.
+
+  Notes
+  -----
+  The values of the first two parameters come from operations in
+  the Connection object. The activation functions with more the two parameters
+  must have the extra parameters passed through the parameter 'fargs_list' of
+  'evodynamic.connection.WeightedConnection'.
+
+  Examples
+  --------
+  >>> evodynamic.connection.WeightedConnection(ca_binary_state,ca_binary_state,
+                                               rule_binary_soc_sca_1d_width3_func,
+                                               ca_conn,
+                                               fargs_list=[(prob_list,)])
+  """
   shape_previous_state = tf.shape(previous_state)
 
-  pattern_0_op = tf.equal(pattern, 0)
-  pattern_1_op = tf.equal(pattern, 1)
-  pattern_2_op = tf.equal(pattern, 2)
-  pattern_3_op = tf.equal(pattern, 3)
-  pattern_4_op = tf.equal(pattern, 4)
-  pattern_5_op = tf.equal(pattern, 5)
-  pattern_6_op = tf.equal(pattern, 6)
-  pattern_7_op = tf.equal(pattern, 7)
-
-  prob_0 = stochastic_prob(prob_list[0], prob_list[8])
-  prob_1 = stochastic_prob(prob_list[1], prob_list[8])
-  prob_2 = stochastic_prob(prob_list[2], prob_list[8])
-  prob_3 = stochastic_prob(prob_list[3], prob_list[8])
-  prob_4 = stochastic_prob(prob_list[4], prob_list[8])
-  prob_5 = stochastic_prob(prob_list[5], prob_list[8])
-  prob_6 = stochastic_prob(prob_list[6], prob_list[8])
-  prob_7 = stochastic_prob(prob_list[7], prob_list[8])
-
-
-  prob_new_state_0 = tf.less_equal(tf.random.uniform(shape_previous_state),\
-                                               prob_0)
-
-  prob_new_state_1 = tf.less_equal(tf.random.uniform(shape_previous_state),\
-                                               prob_1)
-
-  prob_new_state_2 = tf.less_equal(tf.random.uniform(shape_previous_state),\
-                                               prob_2)
-
-  prob_new_state_3 = tf.less_equal(tf.random.uniform(shape_previous_state),\
-                                               prob_3)
-
-  prob_new_state_4 = tf.less_equal(tf.random.uniform(shape_previous_state),\
-                                               prob_4)
-
-  prob_new_state_5 = tf.less_equal(tf.random.uniform(shape_previous_state),\
-                                               prob_5)
-
-  prob_new_state_6 = tf.less_equal(tf.random.uniform(shape_previous_state),\
-                                               prob_6)
-
-  prob_new_state_7 = tf.less_equal(tf.random.uniform(shape_previous_state),\
-                                               prob_7)
-
-  new_state_pattern_0 = tf.where(prob_new_state_0,\
-                                 tf.ones(shape_previous_state, dtype=tf.float64),\
-                                 tf.zeros(shape_previous_state, dtype=tf.float64))
-
-  new_state_pattern_1 = tf.where(prob_new_state_1,\
-                                 tf.ones(shape_previous_state, dtype=tf.float64),\
-                                 tf.zeros(shape_previous_state, dtype=tf.float64))
-
-  new_state_pattern_2 = tf.where(prob_new_state_2,\
-                                 tf.ones(shape_previous_state, dtype=tf.float64),\
-                                 tf.zeros(shape_previous_state, dtype=tf.float64))
-
-  new_state_pattern_3 = tf.where(prob_new_state_3,\
-                                 tf.ones(shape_previous_state, dtype=tf.float64),\
-                                 tf.zeros(shape_previous_state, dtype=tf.float64))
-
-  new_state_pattern_4 = tf.where(prob_new_state_4,\
-                                 tf.ones(shape_previous_state, dtype=tf.float64),\
-                                 tf.zeros(shape_previous_state, dtype=tf.float64))
-
-  new_state_pattern_5 = tf.where(prob_new_state_5,\
-                                 tf.ones(shape_previous_state, dtype=tf.float64),\
-                                 tf.zeros(shape_previous_state, dtype=tf.float64))
-
-  new_state_pattern_6 = tf.where(prob_new_state_6,\
-                                 tf.ones(shape_previous_state, dtype=tf.float64),\
-                                 tf.zeros(shape_previous_state, dtype=tf.float64))
-
-  new_state_pattern_7 = tf.where(prob_new_state_7,\
-                                 tf.ones(shape_previous_state, dtype=tf.float64),\
-                                 tf.zeros(shape_previous_state, dtype=tf.float64))
-
-  update_0_op = tf.where(pattern_0_op, new_state_pattern_0, previous_state)
-  update_1_op = tf.where(pattern_1_op, new_state_pattern_1, update_0_op)
-  update_2_op = tf.where(pattern_2_op, new_state_pattern_2, update_1_op)
-  update_3_op = tf.where(pattern_3_op, new_state_pattern_3, update_2_op)
-  update_4_op = tf.where(pattern_4_op, new_state_pattern_4, update_3_op)
-  update_5_op = tf.where(pattern_5_op, new_state_pattern_5, update_4_op)
-  update_6_op = tf.where(pattern_6_op, new_state_pattern_6, update_5_op)
-  update_7_op = tf.where(pattern_7_op, new_state_pattern_7, update_6_op)
+  update_7_op = rule_binary_sca_1d_width3_func(pattern, previous_state, prob_list)
 
   #return update_7_op
   new_state_op =  tf.cast(tf.less_equal(tf.random.uniform(shape_previous_state),\
@@ -261,8 +445,50 @@ def rule_binary_soc_sca_1d_width3_func(pattern, previous_state, prob_list):
 
   return tf.cond(tf.equal(tf.reduce_mean(previous_state), 1.0), lambda: new_state_op, lambda: update_7_op)
 
-
 def integrate_and_fire(potential_change, spike_in, potential, threshold, potential_decay):
+  """
+  integrate_and_fire(potential_change, spike_in, potential, threshold, potential_decay)
+
+  Activation function for spiking neural networks with neuron model
+  integrate and fire.
+
+  Parameters
+  ----------
+  potential_change : Tensor
+      Change of the membrane potential, which comes from the result of the
+      matrix multiplication that happens in
+      evodynamic.connection.WeightedConnection
+  spike_in : Tensor
+      Input spikes.
+  potential : Tensor
+      Membrane potential state from the group of cells defined by
+      the parameter 'to_group_state' in evodynamic.connection.WeightedConnection.
+  threshold : float
+      Threshold value that triggers the production of an output spike if the
+      value of the membrane potential in higher than the threshold.
+  potential_decay : float
+      Decay value of the membrane potential.
+
+  Returns
+  -------
+  out : Tensor
+      Tensor containing the operation for the output spikes.
+
+  Notes
+  -----
+  The values of the first two parameters come from operations in
+  the Connection object. The activation functions with more the two parameters
+  must have the extra parameters passed through the parameter 'fargs_list' of
+  'evodynamic.connection.WeightedConnection'.
+
+  Examples
+  --------
+  >>> evodynamic.connection.WeightedConnection(spike_state_layer_1,
+                                               spike_state_layer_2, act.integrate_and_fire,
+                                               l1_l2_connection,
+                                               fargs_list=[(potential,threshold,
+                                                            potential_decay)])
+  """
   shape_potential = tf.shape(potential)
   potential_update_op_1 = tf.add(potential, potential_change)
   has_spike_op = tf.greater(potential, threshold)
@@ -276,6 +502,56 @@ def integrate_and_fire(potential_change, spike_in, potential, threshold, potenti
 
 
 def izhikevich(potential_change, spike_in, potential, recovery, a, b, c, d, dt):
+  """
+  izhikevich(potential_change, spike_in, potential, recovery, a, b, c, d, dt)
+
+  Activation function for spiking neural networks with Izhikevich neuron model.
+
+  Parameters
+  ----------
+  potential_change : Tensor
+      Change of the membrane potential, which comes from the result of the
+      matrix multiplication that happens in
+      evodynamic.connection.WeightedConnection
+  spike_in : Tensor
+      Input spikes.
+  potential : Tensor
+      Membrane potential state from the group of cells defined by
+      the parameter 'to_group_state' in evodynamic.connection.WeightedConnection.
+  recovery : float
+      Membrane recovery state from the group of cells defined by
+      the parameter 'to_group_state' in evodynamic.connection.WeightedConnection.
+  a : float
+      Parameter 'a' of Izhikevich model.
+  b : float
+      Parameter 'b' of Izhikevich model.
+  c : float
+      Parameter 'c' of Izhikevich model.
+  d : float
+      Parameter 'd' of Izhikevich model.
+  dt : float
+      Value in milliseconds of the time-steps in simulation.
+
+  Returns
+  -------
+  out : Tensor
+      Tensor containing the operation for the output spikes.
+
+  Notes
+  -----
+  The values of the first two parameters come from operations in
+  the Connection object. The activation functions with more the two parameters
+  must have the extra parameters passed through the parameter 'fargs_list' of
+  'evodynamic.connection.WeightedConnection'.
+
+  Examples
+  --------
+  >>> evodynamic.connection.WeightedConnection(spike_state_layer_1,
+                                               spike_state_layer_2, act.izhikevich,
+                                               l1_l2_connection,
+                                               fargs_list=[(potential,recovery,
+                                                            a,b,c,d,dt)])
+  """
   shape_potential = tf.shape(potential)
   has_spike_op = tf.greater(potential, 30.0)
 
--- evodynamic/cells/cells.py
+++ evodynamic/cells/cells.py
@@ -12,6 +12,26 @@ class Cells(object):
   Class for Cells
   """
   def __init__(self, amount: int, batch_size: int, virtual_shape: Optional[Tuple[int]] = None):
+    """
+    __init__(self, amount: int, batch_size: int, virtual_shape: Optional[Tuple[int]] = None)
+
+    Cells constructor
+
+    Parameters
+    ----------
+    amount : int
+        Number of cells in this group.
+    batch_size : int
+        Batch size of the experiment. It copies the cells by the amount of
+        batches
+    virtual_shape : tuple of ints, optional
+        shape tuple for the shape of the group of cells
+
+    Returns
+    -------
+    out : object
+        New object of class Cells.
+    """
     self.amount = amount
     self.batch_size = batch_size
     self.amount_with_batch = (self.amount, self.batch_size)
@@ -23,6 +43,23 @@ class Cells(object):
     self.update_ops = []
 
   def add_binary_state(self, state_name, init="random"):
+    """
+    add_binary_state(self, state_name, init="random")
+
+    Add binary state to a group of cells.
+
+    Parameters
+    ----------
+    state_name : str
+        Number of cells in this group.
+    init : array, list or {'random', 'central', 'zeros', 'ones', 'reversecentral'}
+        initialization of the cells
+
+    Returns
+    -------
+    out : Tensor
+        Tensor of binary states of the group of cells.
+    """
     if init == "random":
       #np.random.seed(1)
       initial = np.random.randint(2, size=self.amount_with_batch).astype(np.float64)
@@ -54,6 +91,23 @@ class Cells(object):
     return var
 
   def add_n_state(self, state_name, n_state):
+    """
+    add_n_state(self, state_name, n_state)
+
+    Add n-nary state to a group of cells.
+
+    Parameters
+    ----------
+    state_name : str
+        Number of cells in this group.
+    n_state : int
+        total number of unique values in a state of a group of cells.
+
+    Returns
+    -------
+    out : Tensor
+        Tensor of n-ary state of the group of cells.
+    """
     initial = np.random.randint(n_state, size=self.amount_with_batch).astype(np.float64)
     var = tf.get_variable(state_name, initializer=initial)
     self.states[state_name] = var
@@ -89,4 +143,14 @@ class Cells(object):
     return var
 
   def get_shaped_indices(self):
+    """
+    get_shaped_indices(self)
+
+    Get indices of the group of cells.
+
+    Returns
+    -------
+    out : array
+        Array with the indices of the cells in the group.
+    """
     return np.arange(self.amount)
